0c4c8d2a3ab51e83245cc6aa4630490b5305fd39
vishnuchandrappan <cvishnu70@gmail.com>
2021-06-05 08:44:33 +0530

    chore(app): basic setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+
+const fuelType = ["Diesel", "Petrol"];
 
 function App() {
+  const [distance, setDistance] = useState(0);
+  const [selectedFuelType, setSelectedFuelType] = useState("Diesel");
+  const [fuelConsumption, setFuelConsumption] = useState(0);
+
+  const handleDistanceChange = (e) => {
+    setDistance(e.target.value);
+  };
+
+  const handleSelectedType = (e) => {
+    setSelectedFuelType(e.target.value);
+  };
+
+  const handleFuelConsumptionChange = (e) => {
+    setFuelConsumption(e.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container">
+      <h1>Hello world</h1>
+
+      <div className="container form">
+        <div className="form-group">
+          <label htmlFor="distance">Distance travelled</label>
+          <input
+            type="number"
+            id="distance"
+            className="form-group"
+            value={distance}
+            onChange={handleDistanceChange}
+          />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="selectedFuelType">Fuel type</label>
+          <select
+            id="selectedFuelType"
+            onChange={handleSelectedType}
+            value={selectedFuelType}
+          >
+            {fuelType.map((fuelType) => (
+              <option value={fuelType} key={fuelType}>
+                {fuelType}
+              </option>
+            ))}
+          </select>
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="fuelConsumption" className="h1">
+            Fuel consumption
+          </label>
+          <input
+            type="number"
+            id="fuelConsumption"
+            value={fuelConsumption}
+            onChange={handleFuelConsumptionChange}
+          />
+        </div>
+
+        <div className="form-group">
+          <button className="button-outline">Calculate</button>
+        </div>
+      </div>
     </div>
   );
 }
